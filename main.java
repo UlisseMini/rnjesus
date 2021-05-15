2ffda7bf32c4695692d08ebc020197c7ce1cd3f1
Ulisse Mini <ulisse.mini@gmail.com>
2021-05-15 18:58:37 -0400

    Set any guy in the rng stream

--- main.java
+++ main.java
@@ -10,21 +10,45 @@ class Main {
   private static final long ap = 246154705703781L; // a inverse mod 2^48
   private static final long b = 0xBL; // also known as 11
 
-  private static long findSeed(int k) {
-    // we want a seed such that the first nextInt() call returns k.
+  private static long findSeedNextInt(int k) {
+    // nextInt discards the bottom 16 bits, we shift left to avoid that
     long k_shifted = k << 16;
 
-    // solving: a*seed + b = k (mod m)
-    long seed = ap*(k_shifted - b) & ((1L << 48) - 1);
+    return findSeed(k_shifted);
+  }
 
-    seed ^= 0x5DEECE66DL; // invert the xor from initial setSeed
+  private static long findSeedNextInt(int k, int step) {
+    // nextInt discards the bottom 16 bits, we shift left to avoid that
+    long k_shifted = k << 16;
+
+    return findSeed(k_shifted, step);
+  }
+
+  private static long findSeed(long k) {
+    // solving: a*seed + b = k (mod m)
+    // (this effectively inverts the LCG one step)
+    long seed = ap*(k - b) & ((1L << 48) - 1);
+
+    return seed;
+  }
+
+  // like findSeed, but such that a specific step of nextInt() returns k
+  // this can be made O(1) by doing the algebra and simplifying a summation,
+  // but this is more readable. (also I want to start simple :p)
+  private static long findSeed(long k, int step) {
+    long seed = k;
+    for (int i = 0; i < step; i++) {
+      seed = findSeed(seed);
+    }
 
     return seed;
   }
 
   public static void main(String[] args) {
     // Find the seed where the first nextInt() call returns 42
-    long seed = findSeed(42);
+    long seed = findSeedNextInt(42, 3);
+
+    seed ^= 0x5DEECE66DL; // invert the xor from initial setSeed
 
     System.out.println("seed: " + seed);
 
